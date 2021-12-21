69b20ba454c008e05c8a39a1231d8765d57707ed
Gianni Ingurgio <35582965+Gianni-Ingurgio@users.noreply.github.com>
2021-12-21 18:24:25 +0000

    Added proper type based output formatting

--- bookmarklet.js
+++ bookmarklet.js
@@ -19,9 +19,17 @@
 
 	console.stdinfo = console.info;
 	console.info = function(args) {
-		console.stdlog(args);
+		console.stdinfo(args);
 		window.alert("console.info: " + args);
 		evalHistory = "console.info: " + args + "\n" + evalHistory;
+	};
+
+	console.stddir = console.dir;
+	console.dir = function(args) {
+		console.stddir(args);
+		let json = JSON.stringify(args);
+		window.alert("console.dir: " + json);
+		evalHistory = "console.dir: " + json + "\n" + evalHistory;
 	};
 
 	while (true) {
@@ -30,6 +38,7 @@
 			console.log = console.stdlog;
 			console.error = console.stderror;
 			console.info = console.stdinfo;
+			console.dir = console.stddir;
 			break;
 		};
 		let output = "";
@@ -37,13 +46,62 @@
 			output = eval(input);
 			lastEntry = input;
 		} catch(e) {
-			output = e.toString()
+			output = e.toString();
 		}
-		evalHistory = input + " = " + output + "\n" + evalHistory;
+		evalHistory = input + " = " + format(output) + "\n" + evalHistory;
 		result = output;
 	}
 
 	function entry() {
 		return eval(lastEntry);
 	};
+
+	function format(arg) {
+		switch (typeof arg) {
+			case "String":
+				arg = arg.valueOf();
+			case "string":
+				return '"' + arg + '"';
+				break;
+			case "Number":
+				arg = arg.valueOf();
+			case "number":
+				return arg.toString();
+				break;
+			case "Boolean":
+				arg = arg.valueOf();
+			case "boolean":
+				return arg;
+				break;
+			case "Object":
+				arg = arg.valueOf();
+			case "object":
+				if (arg === null) {
+					return "null";
+				}
+				if (arg.constructor === Array) {
+					arg = arg.map(format);
+					return "[" + arg.toString() + "]";
+				}
+				if (arg.constructor === Date) {
+					return arg.toLocaleString();
+				}
+				return JSON.stringify(arg);
+				break;
+			case "Date":
+				return arg.toLocaleString();
+				break;
+			case "Array":
+				arg = arg.map(format);
+				return "[" + arg.toString() + "]";
+				break;
+			case "null":
+				return "null";
+				break;
+			case "undefined":
+				return "undefined";
+				break;
+		}
+		return arg;
+	}
 }())
